043343d53cbfcaaf405909a9f178cdb64070626b
SylmarDev <sylmarssbm@gmail.com>
2026-06-03 19:57:20 -0500

    adding spaces

--- calc/src/ai.ts
+++ calc/src/ai.ts
@@ -2060,8 +2060,8 @@ export function generateMoveDist(damageResults: any[], fastestSide: string, aiOp
             if (moveName == "Belly Drum") {
                 const sitrusRecovery = aiItem == "Sitrus Berry" ? Math.trunc(moves[0].attacker.stats.hp / 4) : 0;
                 const hpAfterBellyDrum = moves[0].attacker.originalCurHP - Math.trunc(moves[0].attacker.stats.hp / 2) + sitrusRecovery;
-                //The dead after belly drum check should check the AI's post-belly-drum HP if the AI is faster, or the AI's current HP if it is slower - the AI is not disincentivized from belly drum failing due to low HP
-                const aiNotDeadAfterBellyDrum = aiFaster?(playerHighestRoll < hpAfterBellyDrum):!aiDeadToPlayer;
+                // If AI faster, it checks if its KO'd after belly drum + an attack. AI does not check belly drum failure case - HoopyFreud
+                const aiNotDeadAfterBellyDrum = aiFaster ? (playerHighestRoll < hpAfterBellyDrum) : !aiDeadToPlayer;
                 if (aiMaxedOutAttack  || moves[0].attacker.originalCurHP - Math.trunc(moves[0].attacker.stats.hp / 2) <= 0) { // useless move
                     moveStringsToAdd.push({
                         move: moveName,
