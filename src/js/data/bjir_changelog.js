2f258977d781adc49a18f5d954d63cc608563ff9
Btara Truhandarien <btara.truhandarien@evenup.ai>
2026-06-06 00:13:15 -0400

    refactor(changelog): reversion bjir-changelog

--- src/js/data/bjir_changelog.js
+++ src/js/data/bjir_changelog.js
@@ -25,20 +25,20 @@ const BJIR_CHANGELOG = [
 	),
 	new ChangelogLine(
 		1,
-		0,
 		1,
+		0,
 		"Add 'Sort by name' option for player box; lexicographic ordering, earlier name first",
 	),
 	new ChangelogLine(
 		1,
-		0,
-		2,
+		1,
+		1,
 		"Keep Pokémon in their respective boxes when reloading",
 	),
 	new ChangelogLine(
 		1,
+		2,
 		0,
-		3,
 		"Add 'Sort by speed' option for player box; descendingly sorts by speed stat then lexicographically by name if speeds are equal, earlier name first)",
 	),
 ];
