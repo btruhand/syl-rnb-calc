46878078075967d17f11c0e878695545d0431f5d
Btara Truhandarien <btara.truhandarien@evenup.ai>
2026-06-05 23:00:49 -0400

    chore(changelog): add 1.0.0 version of bjir changelog

---
+++ src/js/data/bjir_changelog.js
@@ -0,0 +1,26 @@
+const BJIR_CHANGELOG = [
+	new ChangelogLine(
+		1,
+		0,
+		0,
+		"Adds 'Export All' feature for opposing team; speed stat included in all exports",
+	),
+	new ChangelogLine(
+		1,
+		0,
+		0,
+		"Importing Pokémon with a mega evolution in player's roster auto-updates player's mega Pokémon to match the imported base Pokémon's level, nature, and IVs",
+	),
+	new ChangelogLine(
+		1,
+		0,
+		0,
+		"Toggleable OHKO color coding and speed borders for opposing team based on currently chosen player's Pokémon",
+	),
+	new ChangelogLine(
+		1,
+		0,
+		0,
+		"Opposing team roster now shows base form of mega Pokémon",
+	),
+];
