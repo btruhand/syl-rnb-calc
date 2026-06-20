c2aba48aa57752c7bd423c89bcab7afdbe891ca5
Btara Truhandarien <btara.truhandarien@evenup.ai>
2026-06-20 02:12:14 -0400

    chore(changelog): add 1.7.3 and 1.8.0 bjir-changelog

--- src/js/data/bjir_changelog.js
+++ src/js/data/bjir_changelog.js
@@ -89,4 +89,6 @@ const BJIR_CHANGELOG = [
 	new ChangelogLine(1,7,0,"Add 'Reset field on trainer change' option"),
 	new ChangelogLine(1, 7, 1,"Add base form Pokémon for Greninja-Ash"),
 	new ChangelogLine(1, 7, 2,"Fix team slots resetting on auto-detected true doubles when opposing pokemon is clicked. Fix selected player pokemon's information when selected in opposing team dropdowm not being shown properly"),
+	new ChangelogLine(1, 7, 3,"Show color coding and speed border if turned on without having to open color coding options"),
+	new ChangelogLine(1, 8, 0,"Add 'Copy your mon to enemy team' functionality"),
 ];
